6d7031264fa78e4a8b0d0093a98cf81a277c405b
Bijendra Yadav <121555807+LEAGAIZEN@users.noreply.github.com>
2025-04-04 18:35:58 +0530

    Create variable.tf

---
+++ variable.tf
@@ -0,0 +1,14 @@
+variable "aws_region" {
+  description = "AWS region"
+  default     = "us-east-1"
+}
+
+variable "instance_type" {
+  description = "EC2 instance type"
+  default     = "t2.micro"
+}
+
+variable "ami_id" {
+  description = "AMI ID for the EC2 instance"
+  default     = "ami-0c55b159cbfafe1f0"  # Update with your region's AMI ID
+}
